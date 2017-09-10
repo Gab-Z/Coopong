eda4da8a925393776e7c93860376fdb36bb166d9
Gab-Z <glevassor@gmail.com>
2017-09-10 12:53:17 +0200

    update

--- phonegap/www/js/main.js
+++ phonegap/www/js/main.js
@@ -24,6 +24,56 @@ function saveAs (fileData,fileName) {
     });
 }
 
+var ExternalStorageSdcardAccess = function ( _fileHandler, _errorHandler ) {
+
+  var errorHandler = _errorHandler || _defultErrorHandler;
+  var fileHandler = _fileHandler || _defultFileHandler;
+  var root = "file:///";
+
+  return {
+    scanRoot:scanRoot,
+    scanPathList:scanPathList,
+    scanPath:scanPath
+  };
+
+  function scanPath( path ) {
+    window.resolveLocalFileSystemURL(path, _gotFiles, errorHandler );
+  }
+
+  function scanRoot() {
+    scanPath( root );
+  }
+
+  function scanPathList( arrayOfPath ) {
+    arrayOfPath.forEach( function ( p ) {
+      scanPath( p );
+    } );
+  }
+
+  function _gotFiles(entry) {
+    // ? Check whether the entry is a file or a directory
+    if (entry.isFile) {
+      // * Handle file
+      fileHandler( entry );
+    }
+    else {
+      // * Scan directory and add media
+      var dirReader = entry.createReader();
+      dirReader.readEntries( function(entryList) {
+        entryList.forEach( function ( entr ) {
+          _gotFiles( entr );
+        } );
+      }, errorHandler );
+    }
+  }
+
+  function _defultFileHandler(fileEntry){
+    console.log( "FileEntry: " + fileEntry.name + " | " + fileEntry.fullPath );
+  }
+  function _defultErrorHandler(error){
+    console.log( 'File System Error: ' + error.code );
+  }
+};
 
 var champs = [
 "Prénom",
@@ -133,6 +183,13 @@ var testValidity = function(input){
 }
 
 window.onload=function(){
+
+  new ExternalStorageSdcardAccess( fileHandler ).scanPath( "file:///storage/sdcard1/music" );
+  function fileHandler( fileEntry ) {
+      alert( fileEntry.name + " | " + fileEntry.toURL() );
+  }
+
+
   //Check localStorage availability
   if ( ! storageAvailable('localStorage')) {
   	alert("Navigateur incompatible, merci d'utiliser une version à jour de Mozilla Firefox")
